d589872c29c2f9a1f9f0c89c01803d05508e1421
syithmukhtar <syithmukhtar@gmail.com>
2017-02-03 10:44:20 +0900

    Test commit
    - Test commit to clear conflicts

--- src/assets/js/supaagent.js
+++ src/assets/js/supaagent.js
@@ -22,23 +22,67 @@ $(function () {
         //     $(window).scrollTop(0);
         // });
 
-        var fired = false;
+        var triggered = false;
         $(window).scroll(function () {
-            var hT = $('#sa-benefits').offset().top,
+            var hT = $('.benefits-section').offset().top,
                 wS = $(this).scrollTop();
-            if (wS > hT && !fired) {
-                $('#sa-female1-frame').css('position', 'absolute').prependTo('#sa-benefits');
+            if (wS > hT && !triggered) {
+                $('#sa-female1-frame').css('position', 'absolute').prependTo('.benefits-section');
                 $('#sa-female1-before').fadeOut(500);
                 $('#sa-female1-after').fadeIn(500);
                 $('#sa-male').fadeIn(500);
+                triggered = true;
             }
         });
 
-        $('.scene-foreground').slick({
-            slide: '.illustration',
+        var triggered2 = false;
+        $(window).scroll(function () {
+            var hT = $('.tasks-section').offset().top,
+                wS = $(this).scrollTop();
+            if (wS > hT && !triggered2) {
+                $('#sa-male-frame').fadeOut(500);
+                $('#new-accesscard').css('display', 'flex').hide().fadeIn(500, function () {
+                    $(this).animate({
+                        top: '0'
+                    }, 500);
+                });
+                triggered2 = true;
+            }
+        });
+
+        // var triggered3 = false;
+        // $(window).scroll(function () {
+        //     var hT = $('.demography-section').offset().top,
+        //         wS = $(this).scrollTop();
+        //     if (wS > hT && !triggered3) {
+        //         $('.demography-section #sa-female2-frame').css('position', 'fixed');
+        //         triggered3 = true;
+        //     }
+        // });
+
+        $(".scene-background").slick({
+            slide: '.scene-bg',
+            dots: false,
+            appendDots: '.dots-container.testimonial-dots',
+            arrows: true,
             infinite: true,
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            prevArrow: '<svg id="prev-arrow"><use xlink:href="#icon-angle-left" /></svg>',
+            nextArrow: '<svg id="next-arrow"><use xlink:href="#icon-angle-right" /></svg>',
+            asNavFor: '.scene-foreground'
+        });
+
+        $(".scene-foreground").slick({
+            slide: '.scene-fg',
+            dots: false,
+            arrows: false,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            prevArrow: '<svg id="prev-arrow"><use xlink:href="#icon-angle-left" /></svg>',
+            nextArrow: '<svg id="next-arrow"><use xlink:href="#icon-angle-right" /></svg>',
+            asNavFor: '.scene-background'
         });
     }
 });
