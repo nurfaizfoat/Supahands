687033909de261be37d21af1afc3edcfb853f036
syithmukhtar <syithmukhtar@gmail.com>
2017-02-03 19:15:12 +0900

    Update 03.02.17
    - [supaagent.scss] Added scrolling navigation, second female avatar.
    - [supaagent.js] Added scrollTrigger, slick, as a global functions.
    Added scrolling navigation on the side.
    - [supaagent.html] Scrolling navigation and avatar edit: added second
    female avatar set

--- src/assets/js/supaagent.js
+++ src/assets/js/supaagent.js
@@ -2,88 +2,126 @@
 SupaAgent
 **************************************** */
 
-/***** Case Studies: START *****/
+/***** Scroll Trigger: START *****/
 
-/***** Case Studies: END *****/
+function scrollTrigger(sectionClass, addHeight, customFunc) {
+    var triggered = false;
+    $(window).scroll(function () {
+        var hT = $(sectionClass).offset().top + addHeight,
+            wS = $(this).scrollTop();
+        if (wS > hT && !triggered) {
+            customFunc();
+            triggered = true;
+        }
+    });
+}
+
+/***** Scroll Trigger: END *****/
 
 
-/***** Project Flow: START *****/
+/***** Demography Slider: START *****/
+
+function demographySlider() {
+    $(".scene-background").slick({
+        slide: '.scene-bg',
+        dots: false,
+        appendDots: '.dots-container.testimonial-dots',
+        arrows: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<svg id="prev-arrow"><use xlink:href="#icon-angle-left" /></svg>',
+        nextArrow: '<svg id="next-arrow"><use xlink:href="#icon-angle-right" /></svg>',
+        asNavFor: '.scene-foreground'
+    });
+
+    $(".scene-foreground").slick({
+        slide: '.scene-fg',
+        dots: false,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<svg id="prev-arrow"><use xlink:href="#icon-angle-left" /></svg>',
+        nextArrow: '<svg id="next-arrow"><use xlink:href="#icon-angle-right" /></svg>',
+        asNavFor: '.scene-background'
+    });
+}
+
+/***** Demography Slider: END *****/
 
 
+/***** ScrollNav: START *****/
 
-/***** Project Flow: END *****/
+function scrollNav() {
+    var sections = [
+        ['.header-section', 0],
+        ['.benefits-section', 5],
+        ['.role-section', 100],
+        ['.tasks-section', 5],
+        ['.demography-section', 0],
+        ['.cta-section', 50]
+    ];
+
+    $('.scrollnav-button').on('mousedown tap', function (e) {
+        var currIndex = $(this).index();
+        $('.scrollnav-button').removeAttr('data-active');
+        $(this).attr('data-active', '');
+        $('html, body').animate({
+            scrollTop: $(sections[currIndex][0]).offset().top + sections[currIndex][1]
+        }, 500);
+    });
+}
+
+/***** ScrollNav: END *****/
+
 
 /***** Initialize on Load: START *****/
 
-
 $(function () {
     if ($('body').is('.supaagent-page')) {
-        // $(window).on('beforeunload', function () {
-        //     $(window).scrollTop(0);
-        // });
 
-        var triggered = false;
-        $(window).scroll(function () {
-            var hT = $('.benefits-section').offset().top,
-                wS = $(this).scrollTop();
-            if (wS > hT && !triggered) {
-                $('#sa-female1-frame').css('position', 'absolute').prependTo('.benefits-section');
-                $('#sa-female1-before').fadeOut(500);
-                $('#sa-female1-after').fadeIn(500);
-                $('#sa-male').fadeIn(500);
-                triggered = true;
-            }
+        // Reset scroll position when page is reloaded
+        $(window).on('beforeunload', function () {
+            $(window).scrollTop(0);
         });
 
-        var triggered2 = false;
-        $(window).scroll(function () {
-            var hT = $('.tasks-section').offset().top,
-                wS = $(this).scrollTop();
-            if (wS > hT && !triggered2) {
-                $('#sa-male-frame').fadeOut(500);
-                $('#new-accesscard').css('display', 'flex').hide().fadeIn(500, function () {
-                    $(this).animate({
-                        top: '0'
-                    }, 500);
-                });
-                triggered2 = true;
-            }
+        // Header-to-Benefit Trigger
+        scrollTrigger('.benefits-section', null, function () {
+            $('#sa-female1-frame').css('position', 'absolute').prependTo('.benefits-section');
+            $('#sa-female1-before').fadeOut(500);
+            $('#sa-female1-after').fadeIn(500);
+            $('#sa-male').fadeIn(500);
         });
 
-        // var triggered3 = false;
-        // $(window).scroll(function () {
-        //     var hT = $('.demography-section').offset().top,
-        //         wS = $(this).scrollTop();
-        //     if (wS > hT && !triggered3) {
-        //         $('.demography-section #sa-female2-frame').css('position', 'fixed');
-        //         triggered3 = true;
-        //     }
-        // });
-
-        $(".scene-background").slick({
-            slide: '.scene-bg',
-            dots: false,
-            appendDots: '.dots-container.testimonial-dots',
-            arrows: true,
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            prevArrow: '<svg id="prev-arrow"><use xlink:href="#icon-angle-left" /></svg>',
-            nextArrow: '<svg id="next-arrow"><use xlink:href="#icon-angle-right" /></svg>',
-            asNavFor: '.scene-foreground'
+        // Role-to-Tasks Trigger
+        scrollTrigger('.tasks-section', null, function () {
+            $('#sa-male-frame').fadeOut(500);
+            $('#new-accesscard').css('display', 'flex').hide().fadeIn(500, function () {
+                $(this).animate({
+                    top: '0'
+                }, 500);
+                $('#new-accesscard .button.secondary').fadeIn(500);
+            });
         });
 
-        $(".scene-foreground").slick({
-            slide: '.scene-fg',
-            dots: false,
-            arrows: false,
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            prevArrow: '<svg id="prev-arrow"><use xlink:href="#icon-angle-left" /></svg>',
-            nextArrow: '<svg id="next-arrow"><use xlink:href="#icon-angle-right" /></svg>',
-            asNavFor: '.scene-background'
+        // Demography Trigger
+        scrollTrigger('.demography-section', 60, function () {
+            $('#sa-female2-frame').css('position', 'fixed').appendTo('.avatar-container');
         });
+
+        // CTA Avatar
+        scrollTrigger('.cta-section', 40, function () {
+            $('#sa-female2-frame').css('position', 'absolute').css('margin-top', '40px').prependTo('.cta-section');
+            $('#sa-female2-before').fadeOut(500);
+            $('#sa-female2-after').fadeIn(500);
+        });
+
+        // Demography Slider
+        demographySlider();
+
+        // ScrollNav
+        scrollNav();
     }
 });
 
